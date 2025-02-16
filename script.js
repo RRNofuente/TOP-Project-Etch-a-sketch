463eac8fda01c07b6cc98c4120037812dbc1b0c3
RR <rrn091301@gmail.com>
2025-02-16 14:49:40 +0800

    feat: Implement algorithm that changes dimension via input

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ const error = document.createElement("p");
 let dimension = 16;
 let squareDivSize = 960 / dimension;
 
-for (let i = 0; i < dimension; i++) {
-    for (let j = 0; j < dimension; j++) {
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.add("squareDiv");
-        squareDiv.style.flexBasis = `${squareDivSize}px`
-        container.appendChild(squareDiv);
-    }
+createGrid(dimension, squareDivSize);
+
+function createGrid(dimension, squareDivSize) {
+    for (let i = 0; i < dimension; i++) {
+        for (let j = 0; j < dimension; j++) {
+            const squareDiv = document.createElement("div");
+            squareDiv.classList.add("squareDiv");
+            squareDiv.style.flexBasis = `${squareDivSize}px`
+            squareDiv.style.height = "auto";
+            container.appendChild(squareDiv);
+        }
+    }    
 }
+
 
 const squareDivHoverListener = document.querySelectorAll(".squareDiv");
 
@@ -33,5 +39,29 @@ enterBtn.addEventListener("click", () => {
     if (input.value > 100 || input.value < 1 || input.value % Math.round(input.value) != 0) {
         error.textContent = "Must be an integer from 1 to 100. Please try again.";
         header.appendChild(error);
+    } else {
+
+        const removeGrid = document.querySelectorAll(".squareDiv");
+
+        removeGrid.forEach((squareDiv) => {
+            container.removeChild(squareDiv);
+        });
+
+        dimension = input.value;
+        squareDivSize = Math.floor(960 / dimension);
+        input.value = "";
+        input.focus();
+        createGrid(dimension, squareDivSize);
+
+        const squareDivHoverListener = document.querySelectorAll(".squareDiv");
+
+        squareDivHoverListener.forEach(squareDiv => {
+            squareDiv.addEventListener("mouseenter", () => {
+                squareDiv.style.backgroundColor = "red";
+            });
+            squareDiv.addEventListener("mouseleave", () => {
+                squareDiv.style.backgroundColor = "white";
+            });
+        });
     }
 });
